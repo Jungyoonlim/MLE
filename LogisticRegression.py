9cc7fac189182c7d63e2668e9cc738bc5bd6ee71
Joanne <jungyoon.lim99@gmail.com>
2023-11-08 15:44:53 -0800

    ml

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
 class Logistic_Regression:
     def __init__(self,lr=0.001,iters=1000):
         self.w=None
@@ -13,6 +16,16 @@ class Logistic_Regression:
         self.b = 0 
 
         for _ in range(self.iters):
-            predictions = np.dot(X)
+            linear_pred = np.dot(X, self.w) + self.b
+            pred = sigmoid(linear_pred)
 
-    def predict():
+            dw = (1/n_samples) * np.dot(X.T, (pred-y))
+            db = (1/n_samples) * np.sum(pred-y)
+
+            self.w=self.w-self.lr-dw
+            self.b= self.b-self.lr*db
+
+    def predict(self,X):
+        linear_pred = np.dot(X, self.w) + self.b
+        pred = sigmoid(linear_pred)
+
