6f5f4149f16afc0a5dc2f9440e9cbe2224f5fdc2
Joanne <jungyoon.lim99@gmail.com>
2023-11-08 16:22:58 -0800

    decision tree learn

--- MLScratch/DecisionTrees.py
+++ MLScratch/DecisionTrees.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import Counter
 
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
@@ -17,9 +18,24 @@ class DecisionTree:
         self.root=None
 
     def fit(self, X, y):
-
-
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1], self.n_features)
         self.root = self._grow_tree()
     
+    def _grow_tree(self,X,y, depth=0):
+        n_samples, n_feats = X.shape
+        n_labels = np.unique(y)
+
+        # check the stopping criteria 
+        if (depth>=self.max_depth or n_labels==1 or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=)
+
+        # find the best split 
+
+        # create child nodes 
+
+    def _most_commmon_label(self,y):
+        
+
 
     def predict(): 
